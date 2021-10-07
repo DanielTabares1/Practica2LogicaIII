a2356787d9398c419a352df2e58aa9ad6e568a6e
DanielTabares1 <64021707+DanielTabares1@users.noreply.github.com>
2021-10-07 13:56:24 -0500

    Se crea primera vista de la interfaz y permite crear los árboles de las 3 maneras

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,9 +1,65 @@
 package Controller;
 
 import arboles.ArbolBinario;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import lista.NodoDobleAVL;
 
 public class Controller {
+    private ArbolBinario a = new ArbolBinario();
+    private int opcion;
+
+    @FXML
+    private Button crear_Button;
+    @FXML
+    private Button nodos_Button;
+    @FXML
+    private Button aleatorio_Button;
+    @FXML
+    private Button recorridos_Button;
+    @FXML
+    private TextField text_Box_1;
+    @FXML
+    private TextField text_Box_2;
+    @FXML
+    private RadioButton radiob1;
+    @FXML
+    private RadioButton radiob2;
+
+
+    public void crear_action(){
+        String s = text_Box_1.getText();
+        switch (opcion){
+            case 0:
+                a.construirArbolString(s);
+                break;
+            case 1:
+                a.construirArbolTamaño(Integer.parseInt(s));
+                break;
+            case 2:
+                String t = text_Box_2.getText();
+                if(radiob1.isSelected()){
+                    a.construirArbolInordenYPreorden(s,t);
+                }
+                else{
+                    a.construirArbolInordenYPosorden(s,t);
+                }
+            default: break;
+        }
+    }
+
+    public void nodos_action(){
+        opcion = 0;
+    }
+    public void aleatorio_action(){
+        opcion = 1;
+    }
+    public void recorridos_action(){
+        opcion = 2;
+    }
+
     public static void main(String[] args) {
         //String s = "2,4,6,3,5,8,1,7";
         String s = "G,F,H,B,A,D,J,I,K,C,E";
